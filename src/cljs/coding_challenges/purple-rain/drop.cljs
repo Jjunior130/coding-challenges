57183832d9272b24f2f107822c9365b5e4da4711
Jjunior130 <Jjunior130@gmail.com>
2017-01-05 17:49:19 -0500

    Refactored

--- src/cljs/coding_challenges/purple-rain/drop.cljs
+++ src/cljs/coding_challenges/purple-rain/drop.cljs
@@ -3,9 +3,9 @@
            [com.rpl.specter :as sp :refer [view ALL transform setval collect-one putval]]))
 
 (defn make [w]
- (->> {:x (rand w)
-       :y (- (rand 100) 199)
-       :z (rand 20)}
+ (->> {:x (q/random w)
+       :y (q/random -200 -100)
+       :z (q/random 20)}
       (transform [(collect-one :z)
                   :len]
                  #(q/map-range %1 0 20 10 20))
@@ -13,23 +13,25 @@
                   :yspeed]
                  #(q/map-range %1 0 20 1 20))))
 
-(defn fall [yspeed y]
- (+ yspeed y))
+(defn fall [d]
+ (->> d
+      (transform [(collect-one :yspeed) :y] +)
+      (transform [(collect-one :z (view #(q/map-range % 0 20 0 0.2)))
+                  :yspeed]
+                 (fn [grav yspeed]
+                  (+ grav yspeed)))))
+
+(defn loop-edge [d]
+ (->> d
+      (setval :y (q/random -200 -100))
+      (transform [(collect-one :z)
+                  :yspeed]
+                 #(q/map-range %1 0 20 4 10))))
 
 (defn update* [h d]
  (if (> (:y d) h)
-  (->> d
-       (setval :y (- (rand 100) 199))
-       (transform [(collect-one :z)
-                   :yspeed]
-                  #(q/map-range %1 0 20 4 10)))
-  (->> d
-       (transform [(collect-one :yspeed) :y]
-                  fall)
-       (transform [(collect-one :z (view #(q/map-range % 0 20 0 0.2)))
-                   :yspeed]
-                  (fn [grav yspeed]
-                   (+ grav yspeed))))))
+  (loop-edge d)
+  (fall d)))
 
 (defn draw [d]
  (let [thick (q/map-range (:z d) 0 20 1 3)]
