d0a7344bbf007a23df37beaea79ecb1028f98ca6
Jjunior130 <Jjunior130@gmail.com>
2017-01-07 02:07:56 -0500

    Prepared mitosis coding challenge

--- src/cljs/coding_challenges/mitosis/cell.cljs
+++ src/cljs/coding_challenges/mitosis/cell.cljs
@@ -1,3 +1,11 @@
 (ns coding-challenges.mitosis.cell
  (:require [quil.core :as q :include-macros true]
            [com.rpl.specter :as sp :refer [view ALL transform setval collect-one putval]]))
+
+(defn make []
+ {:type 'Cell})
+
+(defn update* [cell])
+
+(defn draw [cell])
+
--- src/cljs/coding_challenges/mitosis/views.cljs
+++ src/cljs/coding_challenges/mitosis/views.cljs
@@ -5,3 +5,34 @@
            [re-com.core :as rc]
            [coding-challenges.mitosis.cell :as cell]
            [com.rpl.specter :as sp :refer [putval ALL transform setval collect-one collect]]))
+
+(def w 600)
+(def h 400)
+
+(defn setup [])
+
+(defn update* [sketch])
+
+(defn draw [sketch])
+
+(q/defsketch mitosis-sketch
+             :setup  setup
+             :update update*
+             :draw   draw
+             :host "mitosis"
+             :no-start true
+             :middleware [m/fun-mode]
+             :size [w h])
+
+(defn view []
+ (reagent/create-class
+  {:reagent-render
+   (fn []
+    [:div
+     [rc/title
+      :label "Mitosis demo"
+      :level :level1] [:br]
+     [:canvas#mitosis {:width w :height h}]])
+   :component-did-mount mitosis-sketch}))
+
+
