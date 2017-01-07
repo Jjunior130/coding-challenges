1ab657eadd520d7291c3bf2b97e825b3cd9ced6e
Jjunior130 <Jjunior130@gmail.com>
2017-01-07 01:54:23 -0500

    Preparing mitosis coding challenge

---
+++ src/cljs/coding_challenges/mitosis/cell.cljs
@@ -0,0 +1,3 @@
+(ns coding-challenges.mitosis.cell
+ (:require [quil.core :as q :include-macros true]
+           [com.rpl.specter :as sp :refer [view ALL transform setval collect-one putval]]))
---
+++ src/cljs/coding_challenges/mitosis/views.cljs
@@ -0,0 +1,7 @@
+(ns coding-challenges.mitosis.views
+ (:require [quil.core :as q :include-macros true]
+           [quil.middleware :as m]
+           [reagent.core :as reagent]
+           [re-com.core :as rc]
+           [coding-challenges.space-invaders.cell :as cell]
+           [com.rpl.specter :as sp :refer [putval ALL transform setval collect-one collect]]))
